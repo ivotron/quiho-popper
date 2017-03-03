5ac345907f064dfbe9ea5b8a6ae9c2a2b82c9d47
Ivo Jimenez <ivo.jimenez@gmail.com>
2017-03-02 16:21:12 -0800

    First commit , adds redis experiment

---
+++ experiments/redis/run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e -x
+
+outdir="results/benchoutput"
+
+# delete previous results
+sudo rm -fr $outdir/*
+
+# get reps
+reps=`cat vars.yml | grep 'repetitions:' | awk '{print $2}'`
+if [ -z "$reps" ]; then
+  echo "Expecting 'repetitions' variable"
+  exit 1
+fi
+
+for i in `seq 1 "$reps"` ; do
+  mkdir -p $outdir/repetition/$i
+  docker run --rm -ti \
+    -v `pwd`/ansible:/experiment \
+    -v `pwd`/vars.yml:/experiment/vars.yml \
+    -v `pwd`/$outdir/repetition/$i:/results \
+    -v $SSH_AUTH_SOCK:/ssh-agent \
+    -e SSH_AUTH_SOCK=/ssh-agent \
+    --workdir=/experiment \
+    --net=host \
+    --entrypoint=/bin/bash \
+    ivotron/ansible:2.2.0.0 -c \
+      "ansible-playbook \
+        -e @vars.yml \
+        -e local_results_path=/results \
+        playbook.yml"
+  sudo rm -fr results/facts/
+  sudo mv $outdir/repetition/$i/facts results/
+done
