f4a0f5563b539de49ff50adaee1642fee76d6232
Ivo Jimenez <ivo.jimenez@gmail.com>
2017-05-08 13:40:04 -0700

    Makes use of new geni-lib docker image
    This reduces the amount of work on our end while requesting cloudlab
    resources

--- experiments/regression/geni/request.py
+++ experiments/regression/geni/request.py
@@ -1,125 +1,55 @@
 #!/usr/bin/env python
 
-from geni.aggregate import cloudlab as cl
-from geni.aggregate.apis import DeleteSliverError
-from geni.aggregate.frameworks import ClearinghouseError
-from geni.minigcf.config import HTTP
-from geni.rspec import pg as rspec
-from geni.util import loadContext
-
-import argparse
-import datetime
-import json
 import os
-import time
+import os.path
+import sys
 import yaml
 
-HTTP.TIMEOUT = 300
+import geni.cloudlab_util as cl
+from geni.rspec import pg as rspec
 
-aggregate = {
-    'utah': cl.Utah,
-    'wisconsin': cl.Wisconsin,
-    'clemson': cl.Clemson,
-    'utahddc': cl.UtahDDC,
-    'apt': cl.Apt
-}
+if not sys.argv[1]:
+    raise Exception("expecting --release or --request")
+action = sys.argv[1]
 
+if action == "--release":
+    cl.release(experiment_name='quiho')
+    sys.exit(0)
+elif action != "--request":
+    raise Exception("expecting --release or --request")
 
-def build_context():
-    e = os.environ
+if not os.path.isdir('/output'):
+    raise Exception("expecting '/output folder'")
 
-    with open('/tmp/context.json', 'w') as f:
-        data = {
-            "framework": "emulab-ch2",
-            "cert-path": e['CLOUDLAB_CERT_PATH'],
-            "key-path": e['CLOUDLAB_CERT_PATH'],
-            "user-name": e['CLOUDLAB_USER'],
-            "user-urn": "urn:publicid:IDN+emulab.net+user+"+e['CLOUDLAB_USER'],
-            "user-pubkeypath": e['CLOUDLAB_KEY_PATH'],
-            "project": e['CLOUDLAB_PROJECT']
-        }
-        json.dump(data, f)
+if not os.path.isfile('/vars.yml'):
+    raise Exception("expecting '/vars.yml file'")
 
-    slice_exp = datetime.datetime.now() + datetime.timedelta(hours=2)
+with open('/vars.yml', 'r') as f:
+    config = yaml.load(f)
 
-    try:
-        ctxt = loadContext("/tmp/context.json",
-                           key_passphrase=e['CLOUDLAB_PASSWD'])
-        ctxt.cf.createSlice(ctxt, "quiho-regression", exp=slice_exp)
-    except ClearinghouseError as e:
-        if 'already a registered slice' not in str(e):
-            raise
-        ctxt.cf.renewSlice(ctxt, "quiho-regression", exp=slice_exp)
+sites = config['machines']['cloudlab']['sites']
 
-    return ctxt
+node = rspec.RawPC("node")
+img = "urn:publicid:IDN+apt.emulab.net+image+schedock-PG0:docker-ubuntu16:0"
+node.disk_image = img
 
+r = rspec.Request()
+r.addResource(node)
 
-def request_resources(ctxt, config):
-    cloudlab_sites = config['machines']['cloudlab']['sites']
+manifests = cl.request(experiment_name='quiho', sites=sites,
+                       request=r, expiration=360)
 
-    node = rspec.RawPC("node")
-    img = "urn:publicid:IDN+apt.emulab.net+image+schedock-PG0:docker-ubuntu16:0"
-    node.disk_image = img
+print("Writing /output/machines file")
+with open('/output/machines', 'w') as f:
+    for m in config['machines'].get('local', []):
+        f.write(m + os.linesep)
 
-    r = rspec.Request()
-    r.addResource(node)
+    for site, manifest in manifests.iteritems():
+        f.write(manifest.nodes[0].hostfqdn)
+        f.write(' ansible_user=' + os.environ['CLOUDLAB_USER'])
+        f.write(' ansible_become=true' + os.linesep)
 
-    manifests = []
-    for site in cloudlab_sites:
-        print("Creating sliver on " + site)
-        manifests += [aggregate[site].createsliver(ctxt, "quiho-regression", r)]
+        with open('/output/{}.xml'.format(site), 'w') as mf:
+            mf.write(manifest.text)
 
-    print("Writing /tmp/machines file")
-    with open('/tmp/machines', 'w') as f:
-        for m in config['machines'].get('local', []):
-            f.write(m + os.linesep)
-
-        for m in manifests:
-            f.write(m.nodes[0].hostfqdn)
-            f.write(' ansible_user=' + os.environ['CLOUDLAB_USER'])
-            f.write(' ansible_become=true' + os.linesep)
-
-    print("Waiting for all nodes to boot")
-    timeout = time.time() + 60*15
-    while True:
-        time.sleep(30)
-        for site in cloudlab_sites:
-            status = aggregate[site].sliverstatus(ctxt, "quiho-regression")
-            if status['pg_status'] != 'ready':
-                break
-
-        if time.time() > timeout:
-            release_resources(ctxt, config)
-            raise Exception("Not all nodes came up after 10 minutes")
-
-
-def release_resources(ctxt, config):
-    for site in config['machines']['cloudlab']['sites']:
-        try:
-            aggregate[site].deletesliver(ctxt, "quiho-regression")
-        except ClearinghouseError:
-            time.sleep(30)
-            aggregate[site].deletesliver(ctxt, "quiho-regression")
-        except DeleteSliverError:
-            continue
-        except:
-            raise
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--request', dest='feature', action='store_true')
-    parser.set_defaults(request=True)
-    parser.add_argument('--release', dest='feature', action='store_true')
-    parser.set_defaults(release=False)
-    args = parser.parse_args()
-
-    with open('/tmp/vars.yml', 'r') as f:
-        config = yaml.load(f)
-
-    ctxt = build_context()
-
-    if args.release:
-        release_resources(ctxt, config)
-    else:
-        release_resources(ctxt, config)
-        request_resources(ctxt, config)
+sys.exit(0)
--- experiments/regression/setup.sh
+++ experiments/regression/setup.sh
@@ -3,16 +3,40 @@ set -e
 
 echo '' > ansible/machines
 
+docker pull ivotron/geni-lib:v0.9.4.6
+
+if [ -z $CLOUDLAB_USER ]; then
+  echo "Expecting CLOUDLAB_USER variable"
+  exit 1
+fi
+if [ -z $CLOUDLAB_PASSWORD ]; then
+  echo "Expecting CLOUDLAB_PASSWORD variable"
+  exit 1
+fi
+if [ -z $CLOUDLAB_PROJECT ]; then
+  echo "Expecting CLOUDLAB_PROJECT variable"
+  exit 1
+fi
+if [ -z $CLOUDLAB_PUBKEY_PATH ]; then
+  echo "Expecting CLOUDLAB_PUBKEY_PATH variable"
+  exit 1
+fi
+if [ -z $CLOUDLAB_CERT_PATH ]; then
+  echo "Expecting CLOUDLAB_CERT_PATH variable"
+  exit 1
+fi
+
 docker run --rm \
   -e CLOUDLAB_USER=$CLOUDLAB_USER \
-  -e CLOUDLAB_PASSWD=$CLOUDLAB_PASSWD \
+  -e CLOUDLAB_PASSWORD=$CLOUDLAB_PASSWORD \
   -e CLOUDLAB_PROJECT=$CLOUDLAB_PROJECT \
-  -e CLOUDLAB_KEY_PATH=$CLOUDLAB_KEY_PATH \
+  -e CLOUDLAB_PUBKEY_PATH=$CLOUDLAB_PUBKEY_PATH \
   -e CLOUDLAB_CERT_PATH=$CLOUDLAB_CERT_PATH \
-  -v $CLOUDLAB_KEY_PATH:$CLOUDLAB_KEY_PATH \
+  -v $CLOUDLAB_PUBKEY_PATH:$CLOUDLAB_PUBKEY_PATH \
   -v $CLOUDLAB_CERT_PATH:$CLOUDLAB_CERT_PATH \
-  -v `pwd`/geni/request.py:/request.py \
-  -v `pwd`/vars.yml:/tmp/vars.yml \
-  -v `pwd`/ansible/machines:/tmp/machines \
-  --entrypoint=/request.py \
+  -v `pwd`/geni/:/output \
+  -v `pwd`/vars.yml:/vars.yml \
+  --entrypoint=/output/request.py \
   ivotron/geni-lib:v0.9.4.6 --request
+
+cp geni/machines ansible/
