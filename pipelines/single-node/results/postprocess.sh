726a56d4eb878baf32ad619af8574cb368a9ccf0
Ivo Jimenez <ivo.jimenez@gmail.com>
2018-01-11 10:45:29 -0700

    Adds post-processing for mysql
    Also consolidates all the runtime-based post-processing into a loop

--- pipelines/single-node/results/postprocess.sh
+++ pipelines/single-node/results/postprocess.sh
@@ -19,70 +19,22 @@ docker run --rm -v $OUTDIR/benchmark:/data/benchmark \
     --filefilter '.*stream.*std.out' \
     --shex " grep 'Copy:\\|Scale:\\|Add:\\|Triad' | sed 's/://' | awk '{ print tolower(\$1)\",\"\$2 }'" \
     ./ >> temp
-
 sed -i -s 's/\(.*\),\(.*\),\(.*\),\(.*\),\(.*\),\(.*\)/\1,\2,\3,\5-\4,\6/' temp
 cat temp >> $CSVOUT
 rm temp
 
-# ssca
+# mysql
 docker run --rm -v $OUTDIR/benchmark:/data/benchmark \
   ivotron/json-to-tabular:v0.0.5 \
-    --filefilter '.*ssca.*runtime' \
-    --shex "sed 's/\(.*\):\(.*\):\(.*\)/\1 \2 \3/' | awk '{ print \"ssca,\"((\$1 * 3600) + (\$2 * 60) + \$3) }'" \
+    --jqexp 'to_entries | map([.key, .value]) | .[] ' \
+    --filefilter '.*mysqlslap.*json' \
     ./ >> all.csv
 
-# hpccg
-docker run --rm -v $OUTDIR/benchmark:/data/benchmark \
-  ivotron/json-to-tabular:v0.0.5 \
-    --filefilter '.*hpccg.*runtime' \
-    --shex "sed 's/\(.*\):\(.*\):\(.*\)/\1 \2 \3/' | awk '{ print \"hpccg,\"((\$1 * 3600) + (\$2 * 60) + \$3) }'" \
-    ./ >> all.csv
-
-# sklearn
-docker run --rm -v $OUTDIR/benchmark:/data/benchmark \
-  ivotron/json-to-tabular:v0.0.5 \
-    --filefilter '.*scikit-learn.*runtime' \
-    --shex "sed 's/\(.*\):\(.*\):\(.*\)/\1 \2 \3/' | awk '{ print \"sklearn,\"((\$1 * 3600) + (\$2 * 60) + \$3) }'" \
-    ./ >> all.csv
-
-# comd
-docker run --rm -v $OUTDIR/benchmark:/data/benchmark \
-  ivotron/json-to-tabular:v0.0.5 \
-    --filefilter '.*comd.*runtime' \
-    --shex "sed 's/\(.*\):\(.*\):\(.*\)/\1 \2 \3/' | awk '{ print \"comd,\"((\$1 * 3600) + (\$2 * 60) + \$3) }'" \
-    ./ >> all.csv
-
-# lulesh
-docker run --rm -v $OUTDIR/benchmark:/data/benchmark \
-  ivotron/json-to-tabular:v0.0.5 \
-    --filefilter '.*lulesh.*runtime' \
-    --shex "sed 's/\(.*\):\(.*\):\(.*\)/\1 \2 \3/' | awk '{ print \"lulesh,\"((\$1 * 3600) + (\$2 * 60) + \$3) }'" \
-    ./ >> all.csv
-
-# miniaero
-docker run --rm -v $OUTDIR/benchmark:/data/benchmark \
-  ivotron/json-to-tabular:v0.0.5 \
-    --filefilter '.*miniaero.*runtime' \
-    --shex "sed 's/\(.*\):\(.*\):\(.*\)/\1 \2 \3/' | awk '{ print \"miniaero,\"((\$1 * 3600) + (\$2 * 60) + \$3) }'" \
-    ./ >> all.csv
-
-# miniamr
-docker run --rm -v $OUTDIR/benchmark:/data/benchmark \
-  ivotron/json-to-tabular:v0.0.5 \
-    --filefilter '.*miniamr.*runtime' \
-    --shex "sed 's/\(.*\):\(.*\):\(.*\)/\1 \2 \3/' | awk '{ print \"miniamr,\"((\$1 * 3600) + (\$2 * 60) + \$3) }'" \
-    ./ >> all.csv
-
-# minife
-docker run --rm -v $OUTDIR/benchmark:/data/benchmark \
-  ivotron/json-to-tabular:v0.0.5 \
-    --filefilter '.*minife.*runtime' \
-    --shex "sed 's/\(.*\):\(.*\):\(.*\)/\1 \2 \3/' | awk '{ print \"minife,\"((\$1 * 3600) + (\$2 * 60) + \$3) }'" \
-    ./ >> all.csv
-
-# zlog
-docker run --rm -v $OUTDIR/benchmark:/data/benchmark \
-  ivotron/json-to-tabular:v0.0.5 \
-    --filefilter '.*zlog.*runtime' \
-    --shex "sed 's/\(.*\):\(.*\):\(.*\)/\1 \2 \3/' | awk '{ print \"zlog,\"((\$1 * 3600) + (\$2 * 60) + \$3) }'" \
-    ./ >> all.csv
+# runtime-based ones
+for w in comd lulesh hpccg miniamr miniaero minife scikit-learn ssca zlog ; do
+  docker run --rm -v $OUTDIR/benchmark:/data/benchmark \
+    ivotron/json-to-tabular:v0.0.5 \
+      --filefilter ".*$w.*runtime" \
+      --shex "sed 's/\(.*\):\(.*\):\(.*\)/\1 \2 \3/' | awk '{ print \"$w,\"((\$1 * 3600) + (\$2 * 60) + \$3) }'" \
+      ./ >> all.csv
+done
